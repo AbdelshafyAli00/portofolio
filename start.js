ba6261d47057071398936d5d2a00d9c3a76ccaed
AbdelshafyAli00 <abdelshafyali38@gmail.com>
2024-10-06 23:52:10 +0400

    Add files via upload

---
+++ start.js
@@ -0,0 +1,17 @@
+// Project 1 //
+// let inp =document.getElementById(`input`);
+// let btn =document.getElementById(`btn`);
+// let para =document.getElementById(`msg`);
+// btn.onclick =function(){
+//      let val =inp.value;
+
+
+
+
+
+//         para.innerHTML =val;
+
+
+
+       // project 2//
+
